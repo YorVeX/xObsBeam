5dec52870a73e0f8a4ffa1fcf47b17145ea76ddf
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-05-09 04:17:32 +0200

    Added libjepg-turbo binary and wrapper class

---
+++ libjpeg-turbo/README.md
@@ -0,0 +1,17 @@
+﻿# libjpeg-turbo wrapper
+This is a C# wrapper class for the [libjpeg-turbo library](https://github.com/libjpeg-turbo/libjpeg-turbo).
+
+## Creating the wrapper class
+It was generated using ClangSharpPInvokeGenerator from [ClangSharp project](https://github.com/dotnet/ClangSharp) with the generate.ps1 script. This is a PowerShell script and therefore is for Windows only.
+
+## Prerequisites
+In order to run the script and recreate the wrapper class the following needs to be fulfilled:
+- Windows
+- have ClangSharp installed (`winget install LLVM.LLVM` in a command line window), version 16.0.1 or later
+- have Git for Windows installed (`winget install Git.Git` in a command line window)
+
+The generated script includes types defined in files in the `ClangSharpAttributes` folder.
+
+## Using the wrapper class
+In order to use the wrapper class the binary libjpeg-turbo library is needed. Currently only Windows is supported. The necessary `turbojpeg.dll` is included in the package [https://sourceforge.net/projects/libjpeg-turbo/files/2.1.91%20%283.0%20beta2%29/libjpeg-turbo-2.1.91-vc64.exe/download](libjpeg-turbo-2.1.91-vc64.exe).
+This DLL needs to be in the OBS bin folder, where also obs64.exe is located.
---
+++ libjpeg-turbo/generate.ps1
@@ -0,0 +1,40 @@
+$turboJpegVersion = "2.1.91" # Version 3 Beta 2
+$filePath = "LibJpegTurbo.cs"
+$sourceFolder = "libjpeg-turbo-$turboJpegVersion"
+
+if (Test-Path -Path $sourceFolder) {
+	Write-Host "Deleting folder: $sourceFolder"
+	Remove-Item -Path $sourceFolder -Recurse -Force
+}
+
+git clone -b $turboJpegVersion https://github.com/libjpeg-turbo/libjpeg-turbo $sourceFolder
+
+ClangSharpPInvokeGenerator `
+	-c single-file preview-codegen generate-macro-bindings unix-types <# configuration for the generator, need unix-types even on Windows so that "unsigned long" becomes nuint and not uint #> `
+	--remap tjregion=TJRegion  <# rename classes breaking naming convention and align with the Java API #>  `
+	--remap tjtransform=TJTransform  <# rename classes breaking naming convention and align with the Java API #>  `
+	--remap tjscalingfactor=TJScalingFactor  <# rename classes breaking naming convention and align with the Java API #>  `
+	--with-using TJRegion=ClangSharp <# central namespace for the generic attributes that all ClangSharp generated classes need #>  `
+	--file .\libjpeg-turbo-2.1.91\turbojpeg.h <# file we want to generate bindings for #>  `
+    -n LibJpegTurbo <# namespace of the bindings #> `
+    --methodClassName TurboJpeg <# class name where to put methods #> `
+    --libraryPath turbojpeg <# name of the DLL #> `
+    -o .\$filePath <# output #>
+
+
+# The file is generated with Unix line endings (probably because of the unix-types parameter), let's correct this here and also use UTF-8 BOM so it's in line with other source files.
+
+# Read the file content
+$fileContent = Get-Content -Path $filePath -Raw
+
+# Convert line endings to Windows format (CRLF)
+$fileContent = $fileContent -replace "`r?`n", "`r`n"
+
+# Convert encoding to UTF-8 with BOM
+$encoding = New-Object System.Text.UTF8Encoding($true)
+$fileContentBytes = $encoding.GetBytes($fileContent)
+$byteOrderMark = [System.Text.Encoding]::UTF8.GetPreamble()
+$fileContentBytesWithBOM = $byteOrderMark + $fileContentBytes
+
+# Write the modified content back to the file
+Set-Content -Path $filePath -Value $fileContentBytesWithBOM -Encoding Byte
