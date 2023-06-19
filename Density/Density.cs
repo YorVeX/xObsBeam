399ecf9ac7c84199ac24ae325ef95f70ecf18a4a
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-06-19 14:55:13 +0200

    Add density base library and wrapper class

---
+++ Density/Density.cs
@@ -0,0 +1,99 @@
+﻿// SPDX-FileCopyrightText: © 2023 YorVeX, https://github.com/YorVeX
+// SPDX-License-Identifier: MIT
+
+using ClangSharp;
+using System.Runtime.InteropServices;
+
+namespace Density
+{
+  public enum DENSITY_ALGORITHM
+  {
+    DENSITY_ALGORITHM_CHAMELEON = 1,
+    DENSITY_ALGORITHM_CHEETAH = 2,
+    DENSITY_ALGORITHM_LION = 3,
+  }
+
+  public enum DENSITY_STATE
+  {
+    DENSITY_STATE_OK = 0,
+    DENSITY_STATE_ERROR_INPUT_BUFFER_TOO_SMALL,
+    DENSITY_STATE_ERROR_OUTPUT_BUFFER_TOO_SMALL,
+    DENSITY_STATE_ERROR_DURING_PROCESSING,
+    DENSITY_STATE_ERROR_INVALID_CONTEXT,
+    DENSITY_STATE_ERROR_INVALID_ALGORITHM,
+  }
+
+  public unsafe partial struct density_context
+  {
+    public DENSITY_ALGORITHM algorithm;
+
+    [NativeTypeName("bool")]
+    public byte dictionary_type;
+
+    [NativeTypeName("size_t")]
+    public nuint dictionary_size;
+
+    public void* dictionary;
+  }
+
+  public unsafe partial struct density_processing_result
+  {
+    public DENSITY_STATE state;
+
+    [NativeTypeName("uint_fast64_t")]
+    public ulong bytesRead;
+
+    [NativeTypeName("uint_fast64_t")]
+    public ulong bytesWritten;
+
+    public density_context* context;
+  }
+
+  public static unsafe partial class Density
+  {
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("uint8_t")]
+    public static extern byte density_version_major();
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("uint8_t")]
+    public static extern byte density_version_minor();
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("uint8_t")]
+    public static extern byte density_version_revision();
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("size_t")]
+    public static extern nuint density_get_dictionary_size(DENSITY_ALGORITHM algorithm);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("uint_fast64_t")]
+    public static extern ulong density_compress_safe_size([NativeTypeName("const uint_fast64_t")] ulong input_size);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    [return: NativeTypeName("uint_fast64_t")]
+    public static extern ulong density_decompress_safe_size([NativeTypeName("const uint_fast64_t")] ulong expected_decompressed_output_size);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern void density_free_context([NativeTypeName("density_context *const")] density_context* context, [NativeTypeName("void (*)(void *)")] delegate* unmanaged[Cdecl]<void*, void> mem_free);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_compress_prepare_context([NativeTypeName("const DENSITY_ALGORITHM")] DENSITY_ALGORITHM algorithm, [NativeTypeName("const bool")] byte custom_dictionary, [NativeTypeName("void *(*)(size_t)")] delegate* unmanaged[Cdecl]<nuint, void*> mem_alloc);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_compress_with_context([NativeTypeName("const uint8_t *")] byte* input_buffer, [NativeTypeName("const uint_fast64_t")] ulong input_size, [NativeTypeName("uint8_t *")] byte* output_buffer, [NativeTypeName("const uint_fast64_t")] ulong output_size, [NativeTypeName("density_context *const")] density_context* context);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_compress([NativeTypeName("const uint8_t *")] byte* input_buffer, [NativeTypeName("const uint_fast64_t")] ulong input_size, [NativeTypeName("uint8_t *")] byte* output_buffer, [NativeTypeName("const uint_fast64_t")] ulong output_size, [NativeTypeName("const DENSITY_ALGORITHM")] DENSITY_ALGORITHM algorithm);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_decompress_prepare_context([NativeTypeName("const uint8_t *")] byte* input_buffer, [NativeTypeName("const uint_fast64_t")] ulong input_size, [NativeTypeName("const bool")] byte custom_dictionary, [NativeTypeName("void *(*)(size_t)")] delegate* unmanaged[Cdecl]<nuint, void*> mem_alloc);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_decompress_with_context([NativeTypeName("const uint8_t *")] byte* input_buffer, [NativeTypeName("const uint_fast64_t")] ulong input_size, [NativeTypeName("uint8_t *")] byte* output_buffer, [NativeTypeName("const uint_fast64_t")] ulong output_size, [NativeTypeName("density_context *const")] density_context* context);
+
+    [DllImport("density", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    public static extern density_processing_result density_decompress([NativeTypeName("const uint8_t *")] byte* input_buffer, [NativeTypeName("const uint_fast64_t")] ulong input_size, [NativeTypeName("uint8_t *")] byte* output_buffer, [NativeTypeName("const uint_fast64_t")] ulong output_size);
+  }
+}
---
+++ Density/generate.ps1
@@ -0,0 +1,34 @@
+# If ClangSharpPInvokeGenerator is missing, just run this from a CMD or PowerShell window:
+# dotnet tool install --global ClangSharpPInvokeGenerator
+# You might also need to run this:
+# winget install LLVM.LLVM
+
+
+$filePath = "Density.cs"
+
+ClangSharpPInvokeGenerator `
+	-c single-file preview-codegen generate-macro-bindings unix-types <# configuration for the generator, need unix-types even on Windows so that "unsigned long" becomes nuint and not uint #> `
+	--with-using density_version_major=ClangSharp <# central namespace for the generic attributes that all ClangSharp generated classes need #>  `
+	--file density_api.h <# file we want to generate bindings for #>  `
+    -n Density <# namespace of the bindings #> `
+    --methodClassName Density <# class name where to put methods #> `
+    --libraryPath density <# name of the DLL #> `
+    -o .\$filePath <# output #>
+
+
+# The file is generated with Unix line endings (probably because of the unix-types parameter), let's correct this here and also use UTF-8 BOM so it's in line with other source files.
+
+# Read the file content
+$fileContent = Get-Content -Path $filePath -Raw
+
+# Convert line endings to Windows format (CRLF)
+$fileContent = $fileContent -replace "`r?`n", "`r`n"
+
+# Convert encoding to UTF-8 with BOM
+$encoding = New-Object System.Text.UTF8Encoding($true)
+$fileContentBytes = $encoding.GetBytes($fileContent)
+$byteOrderMark = [System.Text.Encoding]::UTF8.GetPreamble()
+$fileContentBytesWithBOM = $byteOrderMark + $fileContentBytes
+
+# Write the modified content back to the file
+Set-Content -Path $filePath -Value $fileContentBytesWithBOM -Encoding Byte
