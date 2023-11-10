f8288b1eeffe11d15a421bb371db57bb5f85152d
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-11-10 02:17:31 +0100

    Some code cleanup and helpful warnings

--- src/PeerDiscovery.cs
+++ src/PeerDiscovery.cs
@@ -110,12 +110,7 @@ public partial class PeerDiscovery
     _serverPeer.ConnectionType = ConnectionTypes.Socket;
     _serverPeer.Identifier = serviceIdentifier;
 
-    _udpServer = new UdpClient();
-    _udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
-    _udpServer.Client.Bind(new IPEndPoint(IPAddress.Any, MulticastPort));
-    _udpServer.JoinMulticastGroup(IPAddress.Parse(MulticastGroupAddress));
-    _udpIsListening = true;
-    Task.Run(UdpServerReceiveLoop);
+    StartUdpServer();
     Module.Log($"Peer Discovery server: Started and entered receive loop for {serviceType} \"{serviceIdentifier}\" on {serviceAddress}:{servicePort}.", ObsLogLevel.Debug);
   }
 
@@ -131,12 +126,7 @@ public partial class PeerDiscovery
     _serverPeer.ConnectionType = ConnectionTypes.Pipe;
     _serverPeer.Identifier = serviceIdentifier;
 
-    _udpServer = new UdpClient();
-    _udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
-    _udpServer.Client.Bind(new IPEndPoint(IPAddress.Any, MulticastPort));
-    _udpServer.JoinMulticastGroup(IPAddress.Parse(MulticastGroupAddress));
-    _udpIsListening = true;
-    Task.Run(UdpServerReceiveLoop);
+    StartUdpServer();
     Module.Log($"Peer Discovery server: Started and entered receive loop for {serviceType} \"{serviceIdentifier}\".", ObsLogLevel.Debug);
   }
 
@@ -148,6 +138,16 @@ public partial class PeerDiscovery
     _udpServer.Close();
     _udpServer.Dispose();
     Module.Log("Peer Discovery server: Stopped.", ObsLogLevel.Debug);
+  }
+
+  void StartUdpServer()
+  {
+    _udpServer = new UdpClient();
+    _udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+    _udpServer.Client.Bind(new IPEndPoint(IPAddress.Any, MulticastPort));
+    _udpServer.JoinMulticastGroup(IPAddress.Parse(MulticastGroupAddress));
+    _udpIsListening = true;
+    Task.Run(UdpServerReceiveLoop);
   }
 
   void UdpServerReceiveLoop()
@@ -164,13 +164,13 @@ public partial class PeerDiscovery
 
         if (queryItems.Length == 2 && queryItems[0] == MulticastPrefix && queryItems[1] == "Discover")
         {
-          // send a response to the original sender
-          foreach (var networkInterface in NetworkInterfaces.GetUnicastAddressesWithIds())
+          // send a response to the original sender from every available interface
+          foreach (var unicastAddress in NetworkInterfaces.GetUnicastAddressesWithIds())
           {
-            if (_serviceAddress != IPAddress.Any && _serviceAddress.ToString() != networkInterface.Item1.Address.ToString())
+            if (_serviceAddress != IPAddress.Any && _serviceAddress.ToString() != unicastAddress.Item1.Address.ToString())
               continue;
 
-            var responseBytes = Encoding.UTF8.GetBytes(_serverPeer.ToMulticastString(networkInterface.Item2, networkInterface.Item1.Address.ToString()));
+            var responseBytes = Encoding.UTF8.GetBytes(_serverPeer.ToMulticastString(unicastAddress.Item2, unicastAddress.Item1.Address.ToString()));
             _udpServer.Send(responseBytes, responseBytes.Length, senderEndPoint);
           }
         }
